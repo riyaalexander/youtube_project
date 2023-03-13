51e1ed6995e89e27504957008ff6701a49db0260
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-03-13 09:58:24 -0400

    updated about me, VideosIndex, and navbar

--- src/components/VideosIndex/VideosIndex.jsx
+++ src/components/VideosIndex/VideosIndex.jsx
@@ -1,5 +1,60 @@
+// const VideoList = ({ videos }) => {
+//   return <div className="video-list"></div>;
+// };
+
+// export default VideoList;
+
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import VideoPage from "./Video";
+
+import { fetchVideos } from "./fetch";
+
+// import "/Users/shaniquacoston/Pursuit/youtube-clone/src/styles/VideosIndex.css"
+
 const VideoList = ({ videos }) => {
-  return <div className="video-list"></div>;
-};
+const {id} = useParams();
+const [videosList, setVideosList] = useState([]);
+
+useEffect(() => {
+  fetchVideos()
+  .then((response) => {
+    setVideosList(response)
+  }).catch(console.error((error) => {
+    console.error(error);
+  })) 
+},[id])
+
+
+
+  return (
+    <div>
+      {!videosList ? (console.error("Loading error....")) : (<section className="videos-index-wrapper">
+      <ul className="videos-index">
+        <li>
+         {videos.map((video) => <h3>{video.title}</h3>)}
+          <Link to="/videos/:id" element={<VideoPage />}>
+           
+            </Link>
+        </li>
+      </ul>
+    </section>)}
+    
+    </div>
+  )
+}
 
 export default VideoList;
+
+
+
+
+
+
+
+
+
+
+
+
